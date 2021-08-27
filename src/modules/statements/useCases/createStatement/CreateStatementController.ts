5cdb2daf43113c48a5be2165ea24f11a2bf6fdec
Julia Ramos <juliaramosguedes@gmail.com>
2021-08-27 11:39:51 -0300

    feat(transfer) update controller n route

--- src/modules/statements/useCases/createStatement/CreateStatementController.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.ts
@@ -6,20 +6,31 @@ import { CreateStatementUseCase } from './CreateStatementUseCase';
 enum OperationType {
   DEPOSIT = 'deposit',
   WITHDRAW = 'withdraw',
+  TRANSFER = 'transfer',
 }
 
 export class CreateStatementController {
   async execute(request: Request, response: Response) {
-    const { id: user_id } = request.user;
+    const { id: sender_id } = request.user;
+    let { user_id } = request.params;
     const { amount, description } = request.body;
+    let type: OperationType;
 
-    const splittedPath = request.originalUrl.split('/')
-    const type = splittedPath[splittedPath.length - 1] as OperationType;
+    const isTransferStatement = user_id !== undefined;
+    const splittedPath = request.originalUrl.split('/');
+
+    if (isTransferStatement) {
+      type = splittedPath[splittedPath.length - 2] as OperationType;
+    } else {
+      type = splittedPath[splittedPath.length - 1] as OperationType;
+      user_id = sender_id;
+    }
 
     const createStatement = container.resolve(CreateStatementUseCase);
 
     const statement = await createStatement.execute({
       user_id,
+      sender_id,
       type,
       amount,
       description
